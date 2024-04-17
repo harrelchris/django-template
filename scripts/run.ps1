3ac27be01001c36de12753d156a40676e736bdcb
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-04-17 16:15:31 -0500

    Create run.ps1

---
+++ scripts/run.ps1
@@ -0,0 +1,13 @@
+# Usage .\scripts\run -lan
+
+param(
+    [switch]$lan
+)
+
+.venv\Scripts\activate
+
+if ($lan) {
+    python app\manage.py runserver 0.0.0.0:80
+} else {
+    python app\manage.py runserver localhost:80
+}
