0b639fc47eeeefc5599287ffb5b3d58902cf1189
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-04-17 06:13:33 -0500

    Create users app

--- app/core/settings.py
+++ app/core/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -85,3 +86,5 @@ STATICFILES_DIRS = [
 ]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
