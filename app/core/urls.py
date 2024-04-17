58880d196af33306d435cbca4778b0f3defbf5e2
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-04-17 16:25:00 -0500

    Create error templates

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,9 +1,21 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.views import defaults
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
-    path("__debug__/", include("debug_toolbar.urls")),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns.extend(
+        [
+            path("__debug__/", include("debug_toolbar.urls")),
+            path("400/", defaults.bad_request, kwargs={"exception": Exception("Bad Request")}),
+            path("403/", defaults.permission_denied, kwargs={"exception": Exception("Permission Denied")}),
+            path("404/", defaults.page_not_found, kwargs={"exception": Exception("Not Found")}),
+            path("500/", defaults.server_error),
+        ]
+    )
