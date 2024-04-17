5af72b0be20cd235889d16423a60e476f2712641
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-04-17 17:56:38 -0500

    Lint

--- app/core/urls.py
+++ app/core/urls.py
@@ -18,9 +18,21 @@ if settings.DEBUG:
     urlpatterns.extend(
         [
             path("__debug__/", include("debug_toolbar.urls")),
-            path("400/", defaults.bad_request, kwargs={"exception": Exception("Bad Request")}),
-            path("403/", defaults.permission_denied, kwargs={"exception": Exception("Permission Denied")}),
-            path("404/", defaults.page_not_found, kwargs={"exception": Exception("Not Found")}),
+            path(
+                "400/",
+                defaults.bad_request,
+                kwargs={"exception": Exception("Bad Request")},
+            ),
+            path(
+                "403/",
+                defaults.permission_denied,
+                kwargs={"exception": Exception("Permission Denied")},
+            ),
+            path(
+                "404/",
+                defaults.page_not_found,
+                kwargs={"exception": Exception("Not Found")},
+            ),
             path("500/", defaults.server_error),
         ]
     )
