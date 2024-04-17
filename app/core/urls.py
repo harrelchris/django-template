89f2dae736800b5b96cce490251bf93a5b648015
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-04-17 16:25:10 -0500

    Configure admin site

--- app/core/urls.py
+++ app/core/urls.py
@@ -4,6 +4,11 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 from django.views import defaults
 
+admin.site.index_title = "Site administration"
+admin.site.name = "Django Admin"
+admin.site.site_header = "Django administration"
+admin.site.site_title = "Django site admin"
+
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("admin/", admin.site.urls),
